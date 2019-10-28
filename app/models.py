cac4e4c2e945251c65528860571243168c4a75f8
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-28 10:29:21 +0300

    add blog class with methods to save and get blogs from database

--- app/models.py
+++ app/models.py
@@ -40,3 +40,27 @@ class User(UserMixin,db.Model):
         return f'User {self.username}'
 
 
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    description=db.Column(db.String)
+    like = db.Column(db.Integer)
+    dislike = db.Column(db.Integer)
+    # comments_id =db.Column(db.Integer,db.ForeignKey("comments.id"))
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_blogs(cls):
+        '''
+        gets blogs from the database
+        '''
+        blogs=Blog.query.all()
+
+        return blogs
+
