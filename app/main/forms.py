8151941cd875851a103b9e11ee136756760f4cf6
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-28 11:28:40 +0300

    add Form to enable user to submit blogs

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,PasswordField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User,Blog
+
+class BlogForm(FlaskForm):
+
+    name = StringField('Enter Blog Title ',validators=[Required()])
+    description= TextAreaField('Enter Text ...')
+    submit = SubmitField('Submit')
