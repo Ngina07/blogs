c8d2e35d0fdf95199017fb511d0810fedad311ad
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-28 10:57:05 +0300

    add mail message method to send email to users after registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,7 +5,7 @@ from flask_login import login_user,logout_user,login_required
 from app.models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
-# from ..email import mail_message
+from ..email import mail_message
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
