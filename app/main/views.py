e19cca0df1670bec740012f29df9bf2c6970784b
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-28 11:29:21 +0300

    add method to enable users to post blogs

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,redirect,url_for,request,abort
 from flask_login import login_required,login_user,logout_user,current_user
 from . import main
 from app.models import User,Blog
-# from .forms import PitchForm,CommentForm,UpdateProfile
+from .forms import BlogForm
 from .. import db,photos
 
 @main.route('/')
@@ -13,3 +13,22 @@ def index():
     
 
     return render_template('index.html' ,title=title, blogs=blogs )
+
+@main.route('/blog/new',methods=['GET','POST'])
+@login_required
+def new_blog():
+    '''
+    Method to add new blog
+    '''
+    form= BlogForm()
+    if form.validate_on_submit():
+        name =form.name.data
+        description=form.description.data
+        like=0
+        dislike=0
+        new_blog=Blog(name = name ,description=description,like=like,dislike=dislike)
+        new_blog.save_blog()
+        return redirect(url_for('.index'))
+
+    title= 'Blogs'
+    return render_template('blog.html',blog_form=form, title = title)
