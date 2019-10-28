a23a82a2595172bf59af8f2675d81bc6db5c9447
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-28 10:25:01 +0300

    add load_user method to get user by id when they log in

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
@@ -12,7 +17,7 @@ class User(UserMixin,db.Model):
     email=db.Column(db.String(255),unique=True,index = True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    pitches = db.relationship('Pitch', backref = 'user', lazy = 'dynamic')
+    Blogs = db.relationship('Blog', backref = 'user', lazy = 'dynamic')
     pass_secure = db.Column(db.String(225))
 
 
--- config.py
+++ config.py
@@ -4,13 +4,13 @@ class Config:
 
     SECRET_KEY = 'thisisasecretkey123456'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
