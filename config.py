3094818e5d474e0eb5540a926abbed4774fb9d8f
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-28 09:51:23 +0300

    update user class to allow user to set password and save user to the database

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+class Config:
+
+    SECRET_KEY = 'thisisasecretkey123456'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:1234@localhost/blogs'  
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+# 'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+# export MOVIE_API_KEY=
+export SECRET_KEY=thisisasecret123456
+
+python3.6 manage.py server
