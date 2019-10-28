e72c0a6655087dc2638e0ba2bdb451214ccaf40f
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-28 12:28:55 +0300

    display users blogs on home page

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 # export MOVIE_API_KEY=
-export SECRET_KEY=thisisasecret123456
-
+export SECRET_KEY=thisisasecretkey123456
+export MAIL_USERNAME=phillis.njoroge@strathmore.edu
+export MAIL_PASSWORD=HarleyQuinn
+    
+    
 python3.6 manage.py server
